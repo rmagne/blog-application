2f91732c7030f06ef75c9be2f4c7b394ae4a8e82
Romifff <romain.magne@edhec.com>
2023-10-31 20:20:35 +0100

    initial config

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+//MongoDB config
+const db_uri = process.env.DB_URI;
+
+if (!db_uri) {
+	throw new Error("DB_URI is not defined in .env file");
+}
+
+mongoose
+	.connect(db_uri)
+	.then(() => console.log("Connected to DB"))
+	.catch((err) => console.log(err));
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+	console.log(`Server is Fire at http://localhost:${port}`);
+});
